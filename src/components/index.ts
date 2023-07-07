5e6f32bf399d213c10dbe2b6449f7729c9305ffb
MuhammadSamir1212 <mohamedsamirsea@gmail.com>
2023-07-07 18:43:01 +0300

    first commit

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Hero from "./Hero";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import CarCard from "./CarCard";
+
+export { Hero, SearchBar, CustomFilter, CarCard };
